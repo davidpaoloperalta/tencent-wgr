5d804219b5a0c06c6caf42b9c0d77ac37d7e7fbd
Paolo <peraltadavidpaolo@gmail.com>
2024-04-14 22:51:47 +0800

    adjusted secrets placement

--- backend.tf
+++ backend.tf
@@ -2,5 +2,6 @@ terraform {
   backend "cos" {
     region = "ap-singapore"
     bucket = "dynamic_env-dynamic_proj-state-1324350682"
+    key   = "dynamic_env/dynamic_env-dynamic_proj-state.tfstate"
   }
 }
