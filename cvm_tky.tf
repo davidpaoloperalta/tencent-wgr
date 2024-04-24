0440d20016133e91607fae5a699b63471d78ebba
Paolo <peraltadavidpaolo@gmail.com>
2024-04-25 02:00:41 +0800

    added tky zones

--- cvm_tky.tf
+++ cvm_tky.tf
@@ -1,8 +1,12 @@
+data "tencentcloud_availability_zones" "tky_zones" {
+  provider = tencentcloud.tky
+}
+
 // Bridge
 resource "tencentcloud_instance" "cvm_bridge" {
   provider = tencentcloud.tky
   instance_name     = "${var.env_name}-${var.project}-bridge"
-  availability_zone = data.tencentcloud_availability_zones.zones.zones.0.name
+  availability_zone = data.tencentcloud_availability_zones.tky_zones.zones.0.name
   image_id          = var.bridge_image
   instance_type     = data.tencentcloud_instance_types.instance_types.instance_types.1.instance_type
   system_disk_type  = "CLOUD_BSSD"
@@ -10,24 +14,13 @@ resource "tencentcloud_instance" "cvm_bridge" {
   internet_max_bandwidth_out = 100
   project_id        = tencentcloud_project.project.id
   orderly_security_groups = [tencentcloud_security_group.cvm_security_group_tky.id]
-/*
-  data_disks {
-    data_disk_type = "CLOUD_BSSD"
-    data_disk_size = 20
-    encrypt        = false
-  }
 
-  data_disks {
-    data_disk_type = "CLOUD_BSSD"
-    data_disk_size = 20
-    encrypt        = false
-  }
-*/
 }
 
 resource "tencentcloud_eip" "tky_eip" {
   provider = tencentcloud.tky
   name = "${var.env_name}-${var.project}-pub-for-bridge"
+  internet_max_bandwidth_out = 100
 }
 
 resource "tencentcloud_eip_association" "tky_eip_assoc" {
